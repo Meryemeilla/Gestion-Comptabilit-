2ea65fd71774aaa884032daf4a839092bfbbaa6d
Illa Meryeme <meryemilla477@gmail.com>
2025-12-26 17:59:33 +0100

    chore: Render deploy prep (Gunicorn, Whitenoise, DATABASE_URL support)

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,6 +2,7 @@
 Production settings.
 """
 from .base import *
+import dj_database_url
 
 DEBUG = False
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost'])
@@ -13,3 +14,9 @@ SECURE_SSL_REDIRECT = True
 SECURE_HSTS_SECONDS = 31536000  # 1 year
 SECURE_REFERRER_POLICY = 'same-origin'
 X_FRAME_OPTIONS = 'DENY'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+_db_url = env('DATABASE_URL', default=None)
+if _db_url:
+    DATABASES['default'] = dj_database_url.config(default=_db_url, conn_max_age=600, ssl_require=True)
