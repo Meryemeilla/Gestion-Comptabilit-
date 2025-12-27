c464af584d2c0decbdc75290d4e41251db020e2a
Illa Meryeme <meryemilla477@gmail.com>
2025-12-28 00:20:29 +0100

    Fix Login

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@
 python manage.py migrate --settings=config.settings.prod
 
 # Créer ou mettre à jour le superuser
-python manage.py ensure_superuser --username admin --password Admin123! --email admin@example.com --settings=config.settings.prod
+python manage.py ensure_superuser --username administrateur --password adminn123 --email admin@example.com --settings=config.settings.prod
 
 # Lancer l'application
 exec gunicorn config.wsgi:application --bind=0.0.0.0:$PORT
